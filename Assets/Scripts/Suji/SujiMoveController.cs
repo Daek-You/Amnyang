4480fd8b857b91f8912769960563ed3c106973ef
Daek-You <choieorb1998@naver.com>
2022-02-06 16:12:16 +0900

    Suji Animation(Sargent Jump)
    주인공 캐릭터 수지의 제자리 점프(Sargent Jump)를 구현했습니다.
    - 착지 애니메이션(Suji_Landing) 제작
    - 제자리 점프 애니메이션(Suji_SargentJump) 제작
    - "SujiMoveController" 스크립트 수정 및 리팩토링
    - 점프 구현을 위해, Rigidbody 2D의 Gravity Scale = 3을 줘서 중력 적용
    - 임시 바닥(Floor) 생성
    - "Jumpable_Floor" 레이어(Layer) 생성하여 임시 바닥에 적용 (무한 점프 방지)
    - "Suji_MoveJump" 애니메이션은 제작해놨으나 아직 실제 적용은 하지 않음

--- Assets/Scripts/Suji/SujiMoveController.cs
+++ Assets/Scripts/Suji/SujiMoveController.cs
@@ -6,13 +6,17 @@ public class SujiMoveController : MonoBehaviour
 {
 
     public float walkSpeed;
+    public float jumpPower;
     public Transform sujiTransform;
 
     private Animator animator;
     private Rigidbody2D _rigidBody;
     private float walkDirection;
     private float initScaleX;
+    private const float JUMP_CHARGING_DELAY = 0.55f;      // "서전트 점프" 시 사용
+    private bool hasControl;
     private bool isRunning;
+    private bool isJumping;
 
 
     void Start()
@@ -25,22 +29,60 @@ public class SujiMoveController : MonoBehaviour
     void Update()
     {
         walkDirection = Input.GetAxisRaw("Horizontal");
+        hasControl = !Mathf.Approximately(walkDirection, 0f);
         isRunning = Input.GetButton("Run");
-    }
 
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            Jump();
+        }
+    }
     void FixedUpdate()
     {
-        bool hasControl = !Mathf.Approximately(walkDirection, 0f);
-        TurnOtherSide(hasControl);
-        Move(hasControl);
+        TurnOtherSide();
+        Move();
     }
 
-    private void Move(bool hasControl)   // 0f : Idle, 0.5f : Walk, 1f : Run
+
+    private void Jump() 
     {
+        if (!hasControl && !isJumping)  // 제자리 점프일 경우
+        {
+            isJumping = true;
+            animator.SetBool("Jump", true);
+            Invoke("_Jump", JUMP_CHARGING_DELAY);     
+        }
+        /// 이동 중 점프 구현하기
+
+    }
+
+    private void _Jump()
+    {
+        _rigidBody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Jumpable_Floor"))
+        {
+            if (isJumping)
+            {
+                animator.SetTrigger("Landing");
+                animator.SetBool("Jump", false);
+                isJumping = false;
+            }
+        }
+    }
+    private void Move()
+    {
+        /* 0f : Idle, 0.5f : Walk, 1f : Run  */
+
+        if (isJumping)
+            return;
+
         if (hasControl && isRunning)
         {
             animator.SetFloat("Move", 1f);
-            _rigidBody.velocity = new Vector2(walkDirection * walkSpeed * 2f, _rigidBody.velocity.y);  // 일단 달리기는 걷는 속도의 2배로 해놓음
+            _rigidBody.velocity = new Vector2(walkDirection * walkSpeed * 2f, _rigidBody.velocity.y);
             return;
         }
 
@@ -48,10 +90,12 @@ public class SujiMoveController : MonoBehaviour
         animator.SetFloat("Move", walkValue);
         _rigidBody.velocity = new Vector2(walkDirection * walkSpeed, _rigidBody.velocity.y);
     }
-
-    private void TurnOtherSide(bool hasControl)
+    private void TurnOtherSide()
     {
         if (!hasControl)
+            return;
+
+        if (isJumping)
             return;
 
         var scaleX = sujiTransform.localScale.x;
